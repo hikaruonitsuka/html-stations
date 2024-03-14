9e15d0ee2472b141621a393605b94c2c088359bf
hikaruonitsuka <linlin.work071109@gmail.com>
2024-03-14 17:16:41 +0900

    入門編クリア

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,36 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+
+  const result = await test(userList);
+  return await result;
 }
 
-function test() {
-  return
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+async function test(userList) {
+  await delay(3000);
+  return userList.map(buildFullName);
+}
+
+function buildFullName(user) {
+  user.full_name = `${user.family_name} ${user.first_name}`;
+  return user;
+}
